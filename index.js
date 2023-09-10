4db2e1cf5224b5e473053898fc487c6996d7b380
krystiano13 <crystianotv@gmail.com>
2023-09-10 21:45:05 +0200

    split code into functions

--- index.js
+++ index.js
@@ -6,75 +6,84 @@ let result = "";
 let operator = "";
 let numberID = 0;
 
+const addInput = (e) => {
+  if (e.target.innerText === "." && numbers[numberID].length <= 0) return;
+  numbers[numberID].push(e.target.innerText);
+  input.value = numbers[numberID].join("");
+};
+
+
+const checkOperator = (e) => {
+  switch (e.target.id) {
+    case "+":
+      operator = "+";
+      break;
+    case "-":
+      operator = "-";
+      break;
+    case "x":
+      operator = "x";
+      break;
+    case "/":
+      operator = "/";
+      break;
+  }
+};
+
+const specialFunctions = (e) => {
+  if (e.target.id !== "=") {
+    numberID = 1;
+    input.value = numbers[numberID].join("");
+  }
+
+  if (e.target.id === "ce") {
+    numbers = [[], []];
+    numberID = 0;
+  }
+
+  if (e.target.id === "c" && numbers[numberID].length > 0) {
+    numbers[numberID].pop();
+  }
+};
+
+const calculate = (e) => {
+  if (numbers[0].length > 0 && numbers[1].length > 0) {
+    switch (operator) {
+      case "+":
+        result = Number(numbers[0].join("")) + Number(numbers[1].join(""));
+        break;
+      case "-":
+        result = Number(numbers[0].join("")) - Number(numbers[1].join(""));
+        break;
+      case "x":
+        result = Number(numbers[0].join("")) * Number(numbers[1].join(""));
+        break;
+      case "/":
+        result = Number(numbers[0].join("")) / Number(numbers[1].join(""));
+        break;
+    }
+
+    numbers[0] = result.toString().split("");
+    numbers[1] = [];
+    numberID = 0;
+    input.value = numbers[numberID].join("");
+  }
+};
+
 buttons.forEach((item) => {
   item.addEventListener("click", (e) => {
     if (!e.target.id) {
-      if (e.target.innerText === "." && numbers[numberID].length <= 0) return;
-      numbers[numberID].push(e.target.innerText);
-      input.value = numbers[numberID].join("");
+      addInput(e);
     } else {
-      switch (e.target.id) {
-        case "+":
-          operator = "+";
-          break;
-        case "-":
-          operator = "-";
-          break;
-        case "x":
-          operator = "x";
-          break;
-        case "/":
-          operator = "/";
-          break;
-      }
-
-      if (e.target.id !== "=") {
-        numberID = 1;
-        input.value = numbers[numberID].join("");
-      }
-
-      if (e.target.id === "ce") {
-        numbers = [[], []];
-        numberID = 0;
-      }
-
-      if (e.target.id === "c" && numbers[numberID].length > 0) {
-        numbers[numberID].pop();
-      }
-
+      checkOperator(e);
+      specialFunctions(e);
       if (e.target.id === "=") {
-        if (numbers[0].length > 0 && numbers[1].length > 0) {
-          switch (operator) {
-            case "+":
-              result =
-                Number(numbers[0].join("")) + Number(numbers[1].join(""));
-              break;
-            case "-":
-              result =
-                Number(numbers[0].join("")) - Number(numbers[1].join(""));
-              break;
-            case "x":
-              result =
-                Number(numbers[0].join("")) * Number(numbers[1].join(""));
-              break;
-            case "/":
-              result =
-                Number(numbers[0].join("")) / Number(numbers[1].join(""));
-              break;
-          }
-
-          numbers[0] = result.toString().split("");
-          numbers[1] = [];
-          numberID = 0;
-          input.value = numbers[numberID].join("");
-        }
+        calculate(e);
       }
     }
 
     if (numbers[numberID].length === 0) {
       input.value = "0";
     }
-
-    console.log(numbers);
   });
 });
